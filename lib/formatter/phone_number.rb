316e0426e085c487ae85418a754bc834232df620
callumahern <callum.ahern@simplybusiness.co.uk>
2023-01-11 10:40:16 +0000

    invalid tests pass

--- lib/formatter/phone_number.rb
+++ lib/formatter/phone_number.rb
@@ -3,10 +3,13 @@ require 'pry'
 module Formatter
   class PhoneNumber
 
+    InvalidNumber = 'Error!'
+
     def self.call(number)
       number = number.gsub(' ', '')
       number = number.gsub('+44', '0')
       number = number.gsub('44', '0')
+      raise InvalidNumber
     end
   end
 end
