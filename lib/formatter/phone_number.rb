2d968561f671af7799d85d6aea73e4fa3bd5c900
Emily Harris <emily.harris@simplybusiness.co.uk>
2023-05-10 17:20:16 +0100

    Refactor if statement

--- lib/formatter/phone_number.rb
+++ lib/formatter/phone_number.rb
@@ -5,12 +5,8 @@ module Formatter
     def self.call(number)
       formatted = number.gsub(/ /,'').gsub(/[+]/,'').gsub(/44/,'0')
 
-      if formatted[0..1] == "07"
-        if formatted.length == 11
-          return formatted
-        else
-          raise InvalidNumber
-        end
+      if formatted[0..1] == "07" && formatted.length == 11
+        return formatted
       else
        raise InvalidNumber
      end
